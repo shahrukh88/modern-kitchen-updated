580d3677be82a87b8c3284bea8d36c1c1d0ff0a5
shahrukh88 <shahrukh008877@gmail.com>
2022-07-02 07:30:43 +0500

    Updates

---
+++ precache-manifest.edb6083f21d967f69aa89075d80dd578.js
@@ -0,0 +1,390 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e575ab2078a43544b9f524b3924f7ee5",
+    "url": "/modern-kitchen-updated/index.html"
+  },
+  {
+    "revision": "9954c79aea2c375886d4",
+    "url": "/modern-kitchen-updated/static/css/main.e9f89c51.chunk.css"
+  },
+  {
+    "revision": "62462844c71599a44617",
+    "url": "/modern-kitchen-updated/static/js/2.8cc19a6c.chunk.js"
+  },
+  {
+    "revision": "9686bd4fd02f23782cd70417a0818fda",
+    "url": "/modern-kitchen-updated/static/js/2.8cc19a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9954c79aea2c375886d4",
+    "url": "/modern-kitchen-updated/static/js/main.60b60091.chunk.js"
+  },
+  {
+    "revision": "182759f1dafebb6ce13f",
+    "url": "/modern-kitchen-updated/static/js/runtime-main.ae6c58eb.js"
+  },
+  {
+    "revision": "fcfe6f852267e86a1889a699a35a6ce1",
+    "url": "/modern-kitchen-updated/static/media/Maskgroup1.fcfe6f85.svg"
+  },
+  {
+    "revision": "788035d6f73bab0b4a2cc56475f4b055",
+    "url": "/modern-kitchen-updated/static/media/Maskgroup2.788035d6.svg"
+  },
+  {
+    "revision": "5b07d85b9ee717434ee51a200894f5a1",
+    "url": "/modern-kitchen-updated/static/media/Maskgroup3.5b07d85b.svg"
+  },
+  {
+    "revision": "fcfe6f852267e86a1889a699a35a6ce1",
+    "url": "/modern-kitchen-updated/static/media/Maskgroup4.fcfe6f85.svg"
+  },
+  {
+    "revision": "1ef51700d82e8cc5deabe65a4c8eed0a",
+    "url": "/modern-kitchen-updated/static/media/aboutus.1ef51700.svg"
+  },
+  {
+    "revision": "1965479985a4c5528ab0cc36be64b88f",
+    "url": "/modern-kitchen-updated/static/media/collection.19654799.png"
+  },
+  {
+    "revision": "51e310d133312e2c188444a531ab465b",
+    "url": "/modern-kitchen-updated/static/media/collection1.51e310d1.png"
+  },
+  {
+    "revision": "52c5395781fbed0d416347796226686d",
+    "url": "/modern-kitchen-updated/static/media/collection2.52c53957.png"
+  },
+  {
+    "revision": "d60e3f3898ce07fdc8b14444ed66fc6f",
+    "url": "/modern-kitchen-updated/static/media/contact.d60e3f38.jpg"
+  },
+  {
+    "revision": "f19c5bf288c79216d4c312a8590ce546",
+    "url": "/modern-kitchen-updated/static/media/featurelogo.f19c5bf2.jpg"
+  },
+  {
+    "revision": "3a02526a0c7d47fe64caf7a4434ac8fc",
+    "url": "/modern-kitchen-updated/static/media/footerlogo.3a02526a.svg"
+  },
+  {
+    "revision": "fd27e7dd336c71f9bda22456521ecb2b",
+    "url": "/modern-kitchen-updated/static/media/g2.fd27e7dd.jpg"
+  },
+  {
+    "revision": "a5f7e5a07c954a0c87b3e0efc525e44c",
+    "url": "/modern-kitchen-updated/static/media/g3.a5f7e5a0.jpg"
+  },
+  {
+    "revision": "d38c87536860bec84cecb6e3a40346d4",
+    "url": "/modern-kitchen-updated/static/media/g3.d38c8753.jpg"
+  },
+  {
+    "revision": "20c6def03156c0cc9ef82260cf3b14a1",
+    "url": "/modern-kitchen-updated/static/media/g4.20c6def0.jpg"
+  },
+  {
+    "revision": "efce785c54dcedf085c4557b659b85c2",
+    "url": "/modern-kitchen-updated/static/media/g6.efce785c.jpg"
+  },
+  {
+    "revision": "ce3d89348b2cf6b13b372f04856538bc",
+    "url": "/modern-kitchen-updated/static/media/g7.ce3d8934.jpg"
+  },
+  {
+    "revision": "3cf7b314d23a08e563078f63a08f7fcc",
+    "url": "/modern-kitchen-updated/static/media/g8.3cf7b314.jpg"
+  },
+  {
+    "revision": "e0639770b3c5abf613481b46fe272b04",
+    "url": "/modern-kitchen-updated/static/media/g9.e0639770.jpg"
+  },
+  {
+    "revision": "d947172750cf90ee32ed225229e41b2f",
+    "url": "/modern-kitchen-updated/static/media/gold.d9471727.jpg"
+  },
+  {
+    "revision": "eba198bdd8e028d804f990792b9f6da9",
+    "url": "/modern-kitchen-updated/static/media/gslide.eba198bd.jpg"
+  },
+  {
+    "revision": "d9851109a4fa9ecef71fdf556a968374",
+    "url": "/modern-kitchen-updated/static/media/l1.d9851109.png"
+  },
+  {
+    "revision": "fc55c4679f5f94d77bf4c0df42e47dce",
+    "url": "/modern-kitchen-updated/static/media/l2.fc55c467.png"
+  },
+  {
+    "revision": "009fd107fc00f369f88858408255849b",
+    "url": "/modern-kitchen-updated/static/media/l3.009fd107.png"
+  },
+  {
+    "revision": "100945196cac6140a550e580d62449fe",
+    "url": "/modern-kitchen-updated/static/media/logo-center-4.10094519.jpg"
+  },
+  {
+    "revision": "d0d151e4ba25a16a88f2f07af80bf3e5",
+    "url": "/modern-kitchen-updated/static/media/logo1.d0d151e4.svg"
+  },
+  {
+    "revision": "519f307f588e2d6141dfdc019b530954",
+    "url": "/modern-kitchen-updated/static/media/mark3.519f307f.png"
+  },
+  {
+    "revision": "88be627789be087aa90cb478451d7467",
+    "url": "/modern-kitchen-updated/static/media/mark4.88be6277.png"
+  },
+  {
+    "revision": "868275df5d3e346dc064a1a5109af6de",
+    "url": "/modern-kitchen-updated/static/media/mg.868275df.jpg"
+  },
+  {
+    "revision": "ae253468082f99d52c3f529a24e6e0eb",
+    "url": "/modern-kitchen-updated/static/media/p1.ae253468.jpg"
+  },
+  {
+    "revision": "4ab41dc78c9c6cb3bad1590d4f5f7071",
+    "url": "/modern-kitchen-updated/static/media/p10.4ab41dc7.jpg"
+  },
+  {
+    "revision": "cf1aca3c05aa135ae64c0ee280a0e51d",
+    "url": "/modern-kitchen-updated/static/media/p11.cf1aca3c.jpg"
+  },
+  {
+    "revision": "98991aa96eafd32ae9c94dbf25130054",
+    "url": "/modern-kitchen-updated/static/media/p12.98991aa9.jpg"
+  },
+  {
+    "revision": "f9b968cafff297df51a4491a8308ab9d",
+    "url": "/modern-kitchen-updated/static/media/p13.f9b968ca.jpg"
+  },
+  {
+    "revision": "5dc3dedab9dd1090b425d09ab02e5ebc",
+    "url": "/modern-kitchen-updated/static/media/p14.5dc3deda.jpg"
+  },
+  {
+    "revision": "ee26b18bec0c8fabbc45d1776af44ceb",
+    "url": "/modern-kitchen-updated/static/media/p15.ee26b18b.jpg"
+  },
+  {
+    "revision": "6ab14005c5168219d2c7c2fd1b9ab8b1",
+    "url": "/modern-kitchen-updated/static/media/p16.6ab14005.jpg"
+  },
+  {
+    "revision": "3aa6d23ac5fcd33ebf19802734dafb8e",
+    "url": "/modern-kitchen-updated/static/media/p17.3aa6d23a.jpg"
+  },
+  {
+    "revision": "7d4eb617bddd535580758b400be5a57d",
+    "url": "/modern-kitchen-updated/static/media/p18.7d4eb617.jpg"
+  },
+  {
+    "revision": "1a7fb92288fbbc38eaecff287da118d8",
+    "url": "/modern-kitchen-updated/static/media/p19.1a7fb922.jpg"
+  },
+  {
+    "revision": "7e53f3da32918576e5c5d97e6166dd62",
+    "url": "/modern-kitchen-updated/static/media/p2.7e53f3da.jpg"
+  },
+  {
+    "revision": "b1b7ff1fc30143358e2c8d1fd8610753",
+    "url": "/modern-kitchen-updated/static/media/p20.b1b7ff1f.jpg"
+  },
+  {
+    "revision": "729fdd7e9ecbe2af58760f03c5f300fd",
+    "url": "/modern-kitchen-updated/static/media/p21.729fdd7e.jpg"
+  },
+  {
+    "revision": "ab9f6e9eb5e0b00f7a1ca90ca9d720d5",
+    "url": "/modern-kitchen-updated/static/media/p3.ab9f6e9e.jpg"
+  },
+  {
+    "revision": "ccd0b3a035c9f8484caa54a36cbcfc64",
+    "url": "/modern-kitchen-updated/static/media/p4.ccd0b3a0.jpg"
+  },
+  {
+    "revision": "90d1fc990899c3d313413c53f0c49d98",
+    "url": "/modern-kitchen-updated/static/media/p5.90d1fc99.jpg"
+  },
+  {
+    "revision": "e631c31fa78bd18af0120d46d879a2af",
+    "url": "/modern-kitchen-updated/static/media/p6.e631c31f.jpg"
+  },
+  {
+    "revision": "00472cc394474acbb00e26ad825d68f5",
+    "url": "/modern-kitchen-updated/static/media/p7.00472cc3.jpg"
+  },
+  {
+    "revision": "e5286edbe05f923cb9b822db3051d31c",
+    "url": "/modern-kitchen-updated/static/media/p8.e5286edb.jpg"
+  },
+  {
+    "revision": "521e80065d09e2a2033f7bf48f66145f",
+    "url": "/modern-kitchen-updated/static/media/p9.521e8006.jpg"
+  },
+  {
+    "revision": "a6c3a5f34026175c4dce6cdb5e9097a1",
+    "url": "/modern-kitchen-updated/static/media/pla1.a6c3a5f3.jpg"
+  },
+  {
+    "revision": "913a57c2e16453e5cf5720401ef312bd",
+    "url": "/modern-kitchen-updated/static/media/pla2.913a57c2.jpg"
+  },
+  {
+    "revision": "0930ce96d9c35980cb7c79668fc7ca9c",
+    "url": "/modern-kitchen-updated/static/media/pla3.0930ce96.jpg"
+  },
+  {
+    "revision": "8826506ebb218f5dd69e46a691b057f4",
+    "url": "/modern-kitchen-updated/static/media/pla4.8826506e.jpg"
+  },
+  {
+    "revision": "eecf360516a2b665fdd12c5a1e7291ee",
+    "url": "/modern-kitchen-updated/static/media/pla5.eecf3605.jpg"
+  },
+  {
+    "revision": "c301935863137392ab31a06a270f4816",
+    "url": "/modern-kitchen-updated/static/media/pla6.c3019358.jpg"
+  },
+  {
+    "revision": "4262013d1222d791197f5175475063d4",
+    "url": "/modern-kitchen-updated/static/media/pla7.4262013d.jpg"
+  },
+  {
+    "revision": "b3026625f7e0d57f93924e7d4d04eb31",
+    "url": "/modern-kitchen-updated/static/media/plat.b3026625.jpg"
+  },
+  {
+    "revision": "22efd3cd8e8e53513b9795ec460939aa",
+    "url": "/modern-kitchen-updated/static/media/platfront.22efd3cd.jpg"
+  },
+  {
+    "revision": "71ab7591ce24fdd7ac542b2cdd0de956",
+    "url": "/modern-kitchen-updated/static/media/s1.71ab7591.jpg"
+  },
+  {
+    "revision": "9c7765f78df3fea62bffd110393201a4",
+    "url": "/modern-kitchen-updated/static/media/s1m.9c7765f7.jpg"
+  },
+  {
+    "revision": "199460c60f9da21402afcfb1895dbc75",
+    "url": "/modern-kitchen-updated/static/media/s2.199460c6.jpg"
+  },
+  {
+    "revision": "588934d6ac8e02adc7971d16c5c38d40",
+    "url": "/modern-kitchen-updated/static/media/s2.588934d6.jpg"
+  },
+  {
+    "revision": "a6a881d47680f428d16bb79cd5e00761",
+    "url": "/modern-kitchen-updated/static/media/s21.a6a881d4.jpg"
+  },
+  {
+    "revision": "0407b1b4003fc3ff4b571208cdd11c56",
+    "url": "/modern-kitchen-updated/static/media/s3.0407b1b4.jpg"
+  },
+  {
+    "revision": "c944ad844657ca90bda82ab6532cf379",
+    "url": "/modern-kitchen-updated/static/media/s4.c944ad84.jpg"
+  },
+  {
+    "revision": "e39ef03cf364e98fde959292417e4211",
+    "url": "/modern-kitchen-updated/static/media/s5.e39ef03c.jpg"
+  },
+  {
+    "revision": "fe1edf011e184868055202d1e9a7e557",
+    "url": "/modern-kitchen-updated/static/media/s6.fe1edf01.jpg"
+  },
+  {
+    "revision": "d489b4672ffe7299e507c4fb4130dbae",
+    "url": "/modern-kitchen-updated/static/media/s7.d489b467.jpg"
+  },
+  {
+    "revision": "05cccc494e005d7878de468a461ff60b",
+    "url": "/modern-kitchen-updated/static/media/s8.05cccc49.jpg"
+  },
+  {
+    "revision": "3fee03e2dde64c265f141da18ea76973",
+    "url": "/modern-kitchen-updated/static/media/s9.3fee03e2.jpg"
+  },
+  {
+    "revision": "ef254b7aa4a96e4675b5c94c668d7605",
+    "url": "/modern-kitchen-updated/static/media/silver.ef254b7a.jpg"
+  },
+  {
+    "revision": "d58783a2fcebc8d4075438865431595c",
+    "url": "/modern-kitchen-updated/static/media/silverlogo.d58783a2.jpg"
+  },
+  {
+    "revision": "e430d9b1138dbf220fc513f2c4aa268d",
+    "url": "/modern-kitchen-updated/static/media/t1.e430d9b1.jpg"
+  },
+  {
+    "revision": "fd27e7dd336c71f9bda22456521ecb2b",
+    "url": "/modern-kitchen-updated/static/media/t1.fd27e7dd.jpg"
+  },
+  {
+    "revision": "3825b375484ebfccb035846e32e29b17",
+    "url": "/modern-kitchen-updated/static/media/t2.3825b375.jpg"
+  },
+  {
+    "revision": "19cf3c9b002799175307ba276853157e",
+    "url": "/modern-kitchen-updated/static/media/t3.19cf3c9b.jpg"
+  },
+  {
+    "revision": "d38c87536860bec84cecb6e3a40346d4",
+    "url": "/modern-kitchen-updated/static/media/t3.d38c8753.jpg"
+  },
+  {
+    "revision": "20c6def03156c0cc9ef82260cf3b14a1",
+    "url": "/modern-kitchen-updated/static/media/t4.20c6def0.jpg"
+  },
+  {
+    "revision": "c481d524edd7329446b11091d14c6acd",
+    "url": "/modern-kitchen-updated/static/media/t5.c481d524.jpg"
+  },
+  {
+    "revision": "efce785c54dcedf085c4557b659b85c2",
+    "url": "/modern-kitchen-updated/static/media/t6.efce785c.jpg"
+  },
+  {
+    "revision": "4e40785d99b1118b34da28b895ff2a71",
+    "url": "/modern-kitchen-updated/static/media/t7.4e40785d.jpg"
+  },
+  {
+    "revision": "1807320c01cccfacb0b7acbd7d1a3763",
+    "url": "/modern-kitchen-updated/static/media/t8.1807320c.jpg"
+  },
+  {
+    "revision": "5cca8c5c590310736b3fee2bd1b3f8e7",
+    "url": "/modern-kitchen-updated/static/media/t9.5cca8c5c.jpg"
+  },
+  {
+    "revision": "cc948de97d412191f6cd8ae45249695c",
+    "url": "/modern-kitchen-updated/static/media/term.cc948de9.jpg"
+  },
+  {
+    "revision": "891d2d89a8c1d74a9c7888a8721ff4a7",
+    "url": "/modern-kitchen-updated/static/media/titanium.891d2d89.jpg"
+  },
+  {
+    "revision": "5104a5b009ac433ca8f23d219f29a4ca",
+    "url": "/modern-kitchen-updated/static/media/tslide.5104a5b0.jpg"
+  },
+  {
+    "revision": "e303504312b62587c36006ec7dbcdfeb",
+    "url": "/modern-kitchen-updated/static/media/white.e3035043.jpg"
+  },
+  {
+    "revision": "f5ed2207f4c09457694a3f6808770e8f",
+    "url": "/modern-kitchen-updated/static/media/white2.f5ed2207.jpg"
+  },
+  {
+    "revision": "e8bddabd6a62502e97c98ab8f7fcda0c",
+    "url": "/modern-kitchen-updated/static/media/white8.e8bddabd.jpg"
+  },
+  {
+    "revision": "46ed8a4ec863fdef2abb15af3d209380",
+    "url": "/modern-kitchen-updated/static/media/whites.46ed8a4e.jpg"
+  }
+]);
